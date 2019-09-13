da91c317431df9fa85d11a2ed7d03b6380b7f557
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2019-09-13 18:23:33 -0300

    get token

--- get-answer.php
+++ get-answer.php
@@ -1,6 +1,14 @@
 <?php
 
+require 'vendor/autoload.php';
+
 // get token
+$client = new GuzzleHttp\Client();
+$responseToken = $client->get('http://applicant-test.us-east-1.elasticbeanstalk.com/');
+
+$domDoc = new DOMDocument();
+$domDoc->loadHTML($responseToken->getBody()->getContents());
+$tokenElement = $domDoc->getElementById('token');
 
 // parse token
 
