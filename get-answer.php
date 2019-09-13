58db732ec235026cae9cf42ec10959a8798e4cac
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2019-09-13 18:24:36 -0300

    parse token

--- get-answer.php
+++ get-answer.php
@@ -11,5 +11,9 @@ $domDoc->loadHTML($responseToken->getBody()->getContents());
 $tokenElement = $domDoc->getElementById('token');
 
 // parse token
+$find = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+$replace = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'];
+$token = str_replace($find, $replace, $tokenElement->getAttributeNode('value')->textContent);
+
 
 // send token and get answer
